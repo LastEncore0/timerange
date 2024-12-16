6277f952a73579d57108c70748733e286660b465
Last Encore <98925995+LastEncore0@users.noreply.github.com>
2024-12-17 08:32:11 +0900

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace timerange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.ComboBox();
+            this.Start = new System.Windows.Forms.ComboBox();
+            this.End = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ResultText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.button1.Location = new System.Drawing.Point(216, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "調べる";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Input
+            // 
+            this.Input.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Input.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.Input.FormattingEnabled = true;
+            this.Input.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.Input.Location = new System.Drawing.Point(237, 12);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(117, 30);
+            this.Input.TabIndex = 1;
+            // 
+            // Start
+            // 
+            this.Start.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Start.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.Start.FormattingEnabled = true;
+            this.Start.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.Start.Location = new System.Drawing.Point(116, 86);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(117, 30);
+            this.Start.TabIndex = 2;
+            // 
+            // End
+            // 
+            this.End.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.End.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.End.FormattingEnabled = true;
+            this.End.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.End.Location = new System.Drawing.Point(358, 84);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(117, 30);
+            this.End.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.label1.Location = new System.Drawing.Point(243, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "終了時刻";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.label2.Location = new System.Drawing.Point(3, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "開始時刻";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "時刻を指定してください";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // ResultText
+            // 
+            this.ResultText.AutoSize = true;
+            this.ResultText.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.ResultText.Location = new System.Drawing.Point(68, 161);
+            this.ResultText.Name = "ResultText";
+            this.ResultText.Size = new System.Drawing.Size(84, 22);
+            this.ResultText.TabIndex = 8;
+            this.ResultText.Text = "結果は：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 375);
+            this.Controls.Add(this.ResultText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Input;
+        private System.Windows.Forms.ComboBox Start;
+        private System.Windows.Forms.ComboBox End;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label ResultText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace timerange
+{
+    public partial class Form1 : Form
+    {
+        private bool result = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Input.SelectedIndex = 0;
+            Start.SelectedIndex = 0;
+            End.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int InputValue = int.Parse(Input.SelectedItem.ToString());
+            int StartValue = int.Parse(Start.SelectedItem.ToString());
+            int EndValue = int.Parse(End.SelectedItem.ToString());
+
+            result = false;
+
+            if (EndValue > StartValue)
+            {
+                if (InputValue >= StartValue && InputValue < EndValue)
+                {
+                    result = true;
+                    //Console.WriteLine("案例１：" + result);
+                }
+            }
+
+            if (EndValue < StartValue)
+            {
+                if (InputValue >= StartValue || InputValue < EndValue)
+                {
+                    result = true;
+                    //Console.WriteLine("案例２：" + result);
+                }
+            }
+            if (EndValue == StartValue && InputValue == StartValue)
+            {
+                result = true;
+                //Console.WriteLine("案例３：" + result);
+            }
+            if (result==true)
+            {
+                ResultText.Text = "結果は：範囲内です。";
+            }
+            else
+            {
+                ResultText.Text = "結果は：範囲外です。";
+            }
+        }
+    }
+}
